f103d25f7a9008abdceb9a5bc1de00d731a0be33
Willian Stein <willian.sanini@hotmail.com>
2024-02-19 23:59:59 -0300

    Projeto ainda não finalizado

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Alert para o botão de "Fale Conosco"
+    document.getElementById("fale-conosco").onclick = alerta;
+
+    function alerta() {
+        alert("Página em manutenção, desculpe o transtorno!");
+    }
+});
+
+
+
+    
